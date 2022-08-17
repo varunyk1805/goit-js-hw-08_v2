3514bd053f7e9ac63394fc3df1cf9949f0ca3b4f
varunyk1805 <varunyk1805@gmail.com>
2022-08-17 22:08:44 +0300

    add success task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = ({ seconds }) => {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then()
+  .catch(error => {
+    switch (error.name) {
+      case 'RangeError':
+        alert('the time was less than 0 or greater than the video’s duration');
+        break;
+
+      default:
+        alert('some other error occurred');
+        break;
+    }
+  });
