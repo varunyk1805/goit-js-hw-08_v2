a350afa7abeaf6ef9364938561af8356c7391c75
varunyk1805 <varunyk1805@gmail.com>
2022-08-18 22:21:34 +0300

    add success hw_8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const throttle = require('lodash.throttle');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const feedbackFormState = JSON.parse(
+  localStorage.getItem(LOCALSTORAGE_KEY)
+) || { email: '', message: '' };
+
+emailInput.value = feedbackFormState.email;
+messageInput.value = feedbackFormState.message;
+
+const onSave = ({ target: { name, value } }) => {
+  feedbackFormState[name] = value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormState));
+};
+
+const onSubmit = event => {
+  event.preventDefault();
+  console.log(feedbackFormState);
+  feedbackFormState.email = '';
+  feedbackFormState.message = '';
+  event.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+
+feedbackForm.addEventListener('input', throttle(onSave, 500));
+feedbackForm.addEventListener('submit', onSubmit);
